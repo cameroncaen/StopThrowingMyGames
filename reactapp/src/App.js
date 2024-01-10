d129604f0b972945fb35f20114a1d1f582e6fd1d
cameroncaen <cameron.caen@wsu.edu>
2024-01-09 17:48:04 -0800

    Added search bar to base page. Added more styling.
    Search bar does not have correct routing nor styling, this is a rough input.

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,20 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
 import "@fontsource/league-spartan";
+import "@fontsource/be-vietnam-pro";
+import ping from './qping.png';
+import search from './magnifyingglass.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
+        <h2>
           Stop Throwing My Games
-        </p>
+        </h2>
         <p className="subtitle">
-          Don't get <img src="qping.png" meta="Question Mark Ping"></img>'d.
+          Don't get <img src={ping} alt="Question Mark Ping" width="50" height="50" />'d.
         </p>
       </header>
+
+      <article className="App-body">
+        <div class="search-container">
+          <form>
+            <input className="ID-search" name="summonerid" placeholder="Summoner ID" />
+            <input className="tag-search" name="summonertag" placeholder="#" />
+            <button className="search-button" type="submit">
+              <img src={search} alt="Search" width="50" height="50" />
+            </button>
+          </form>
+        </div>
+      </article>
     </div>
   );
 }
+
+// search button does not route to correct route atm; change later
 
 export default App;
