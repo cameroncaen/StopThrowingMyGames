307005b010ecc8ed6b8a560e2058859fc14b792d
Ryan T <rjtrimble22@gmail.com>
2024-01-09 18:19:13 -0800

    Commented the models file for future editing
    Commenting files is the key to good teamwork!

--- MatchupApp/models.py
+++ MatchupApp/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+# Player class containing all of the basic information needed for ALL players in the lobby
 class Player(models.Model):
     username = models.CharField(max_length=25)
     level = models.IntegerField(default=0)
@@ -16,9 +17,16 @@ class Player(models.Model):
     def __str__(self):
         return self.username
 
+# This class "promotes" a player class into a MainPlayer which will have additional stats to
+# view as they will be for the user of the webpage and their specific matchup in game
 class MainPlayer(Player):
+    #Others important stats imported from riot database will be stored here
     forward = models.IntegerField()
 
+
+# This class stores important blurbs (tags) generated from observations of matchup stats. They
+# contain the player being mentioned, the text describing them, and the importance (weight) of 
+# the blurb to determine where on the webpage/heierarchy of stats it should go
 class Tag(models.Model):
     player = models.ForeignKey(Player, on_delete=models.CASCADE)
     tag_text = models.CharField(max_length=100)
