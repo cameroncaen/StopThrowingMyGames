771c0fce2249297d357fafb771978b94735b9fe2
cameroncaen <cameron.caen@wsu.edu>
2024-01-15 20:39:17 -0800

    Started to reform main page w/o fonts or images.
    Need to figure out loader to get these to load but not a huge deal atm.

--- MatchupApp/MatchupApp/settings.py
+++ MatchupApp/MatchupApp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api.apps.ApiConfig',
     'rest_framework',
-    'frontend.apps.FrontendConfig'
+    'frontend.apps.FrontendConfig',
+    'webpack_loader'
 ]
 
 MIDDLEWARE = [
@@ -124,3 +126,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Where the Webpack generated files will be / status
+
+WEBPACK_LOADER = {
+    'DEFAULT': {
+        'BUNDLE_DIR_NAME': 'dist/',
+        'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats.json'),
+    }
+}
--- MatchupApp/frontend/src/components/App.js
+++ MatchupApp/frontend/src/components/App.js
@@ -1,10 +1,36 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
 import HomePage from "./HomePage";
+//import "@fontsource/league-spartan";
+//import "@fontsource/be-vietnam-pro";
+//import ping from '../../static/images/qping.png';
+//import search from '../../static/images/magnifyingglass.png';
 
 export const App = () => {
     return (
-        <h1>BLLLAARRRSSHHH</h1>
+        <div>
+            <header className="App-header">
+                <h2>Stop Throwing My Games</h2>
+                <a className="subtitle">Don't get 'd.</a>
+            </header>
+
+            <article className="App-body">
+                <form>
+                    <div class="search-wrapper">
+                        <div class="search-container">
+                            <input className="ID-search" name="summonerid" placeholder="Summoner Name" />
+                            <label className="search-line" />
+                            <label className="tag-label">#</label>
+                            <input className="tag-search" name="summonertag" maxlength="5" />
+                            <button className="search-button" type="submit">
+                                
+                            </button>
+                        </div>
+                    </div>
+                </form>
+                <p className="na-only">Works for NA summoners only.</p>
+            </article>
+        </div>
     )
 }
 
@@ -12,3 +38,8 @@ export default App
 
 const appDiv = document.getElementById("app")
 render(<App />, appDiv);
+
+// these two images will be in the frontend, however image loading not working yet
+// <img src={ping} alt="Question Mark Ping" width="50" height="50" />
+// <img src={search} alt="Search" width="30" height="30" />
+// fonts also not working rn LOL
