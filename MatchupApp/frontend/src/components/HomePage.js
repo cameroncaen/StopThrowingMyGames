3927dbbd48d1415186bc4784a3c115ed47816ee8
cameroncaen <cameron.caen@wsu.edu>
2024-01-23 17:07:11 -0800

    Updated loading for home screen.

--- MatchupApp/frontend/src/components/HomePage.js
+++ MatchupApp/frontend/src/components/HomePage.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { useNavigate } from 'react-router-dom';
 
+import ping from '../../static/images/qping.png';
+import search from '../../static/images/magnifyingglass.png';
+
 export const HomePage = () => {
     const navigate =  useNavigate();
     
@@ -11,13 +14,20 @@ export const HomePage = () => {
         <div>
             <header className="App-header">
                 <h2>Stop Throwing My Games </h2>
-                <a className="subtitle">Don't get 'd.</a>
+                <a className="subtitle">Don't get <img src={ping} alt="Question Mark Ping" width="50" height="50" />'d.</a>
             </header>
             <article className="App-body">
-            <button 
-                className="search-button" 
-                onClick={navigateSumSearch}
-            >Player Matchup Analysis </button>
+                <div class="search-wrapper">
+                    <div class="search-container">
+                        <button 
+                            className="search-button" 
+                            onClick={navigateSumSearch}
+                        >
+                            <img src={search} alt="Search" width='30' height='30' />
+                        </button>
+                    </div>
+                </div>
+                <p className="na-only">Works for NA summoners only.</p>
             </article>
         </div>
     )
